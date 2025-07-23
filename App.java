f5cb52d6e3d780cb97fd8e43d75962f464921850
sreeja4344 <2100031306cseh@gmail.com>
2025-07-23 23:19:23 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,12 @@
+class App {
+    // if you want to run a java program, class and main method 
+    // are must
+    // public - access modifier
+    // static - no object is required to run program
+    // void - retrun nothing
+    // main - method name, String[] args
+    public static void main(String[] args) {
+        // method body
+        System.out.println("Hello World V3");
+    }
+}
